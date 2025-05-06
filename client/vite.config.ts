970cec9b3e0fcbcb878beea21beddf2ab7c32d9b
Thays Moia Ribeiro <thaysmoiaribeiro@MacBookPro.lan>
2025-05-06 14:26:52 -0700

    Set up app to deploy on Render

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,12 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true,
+    port: 10000,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
-        secure: false,
         changeOrigin: true
       }
     }
